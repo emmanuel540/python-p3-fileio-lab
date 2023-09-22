5e40a7ea35f908e1d8f7185af20deeb87754fd41
emmanuel540! <emmanuelhillary24@gmail.com>
2023-09-22 11:34:42 +0300

    Done and dusted

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,31 @@
 def write_file(file_name, file_content):
-    pass
+    # Add the .txt extension to the file_name
+    file_name_with_extension = str(file_name) + ".txt"
+
+    # Open the file for writing
+    with open(file_name_with_extension, "w") as file:
+        # Write the content to the file
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+     # Add the .txt extension to the file_name
+    file_name_with_extension = str(file_name) + ".txt"
+
+    # Open the file for appending
+    with open(file_name_with_extension, "a") as file:
+        # Append the content to the file
+        file.write(append_content)
 
 def read_file(file_name):
+    # Add the .txt extension to the file_name
+    file_name_with_extension = str(file_name) + ".txt"
+
+    try:
+        # Open the file for reading
+        with open(file_name_with_extension, "r") as file:
+            # Read the content of the file
+            file_content = file.read()
+            return file_content
+    except FileNotFoundError:
+        return "File not found."
     pass
